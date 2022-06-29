e84aaf0d47d392aa503bc689f0c408361e238c86
Henry Doan <henrydoan96@gmail.com>
2022-06-28 20:51:22 -0600

    note back end done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   
   namespace :api do 
-    resources :cats 
+    resources :cats do
+      resources :notes 
+    end
+
+    
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
+Note.all.delete_all
 Cat.all.delete_all 
 User.all.delete_all 
 
 @i = 1 
+@subjects = ['Health', 'Diet', 'Play', 'Other']
 
 5.times do
   @user = User.create(
@@ -14,12 +16,20 @@ User.all.delete_all
   @i += 1
 
   10.times do
-    Cat.create(
+    @cat = Cat.create(
       name: Faker::Creature::Cat.name,
       breed: Faker::Creature::Cat.breed,
       registry: Faker::Creature::Cat.registry,
       avatar: Faker::Avatar.image(slug: 'cat', size: '100x400', format: 'png', set: 'set4'), 
       user_id: @user.id
+    )
+
+    Note.create(
+      subject: @subjects.sample,
+      body: Faker::Lorem.paragraph,
+      ndate: Faker::Date.forward(days: 23),
+      ntime: Faker::Time.forward(days: 23, period: :morning),
+      cat_id: @cat.id
     )
   end
 
